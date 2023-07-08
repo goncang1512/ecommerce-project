2a3a03aff96827f080459d99905eab95426ccfd2
goncang1512 <samuderanst@gmail.com>
2023-07-08 16:14:22 +0700

    new update

--- src/script/keranjang.js
+++ src/script/keranjang.js
@@ -12,18 +12,27 @@ fetch(url)
   .catch((error) => console.log(error));
 
 function renderProducts(cart) {
-  const container = document.querySelector(".menu-cart");
+  const container = document.querySelector(".keranjang-pesanan ");
   container.innerHTML = "";
 
   cart.forEach((item) => {
     const productHTML = `
-         <div class="w-full bg-red-500 flex justify-evenly">
-            <p>1</p>
-            <img src=${item.image} alt="" class="w-12"/>
-            <div>${item.title}</div>
-            <p>jumlah</p>
-            <p>${item.price}</p>
-         </div>
+    <div
+    class="keranjang-self flex w-full justify-evenly items-center h-28 text-white bg-green-600"
+    >
+      <p><input type="checkbox" id="${item.id}" class="cart-checkbox w-5 h-5 outline-0" /></p>
+      <p>
+        <img
+          src=${item.image}
+          alt=""
+          class="h-24 w-24 object-cover"
+        />
+      </p>
+      <p class="cart-title text-sm">${item.title}</p>
+      <p>$ ${item.price}</p>
+      <p>1</p>
+      <p>$ ${item.price}</p>
+    </div>
       `;
     container.insertAdjacentHTML("beforeend", productHTML);
   });
@@ -33,3 +42,5 @@ var kembaliButton = document.getElementById("kembaliButton");
 kembaliButton.addEventListener("click", function () {
   history.back();
 });
+
+// Temukan semua elemen checkbox
