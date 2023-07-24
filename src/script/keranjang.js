6d79a7bcef3482500548462a650f9aa7089eca2e
goncang1512 <samuderanst@gmail.com>
2023-07-24 09:52:46 +0700

    New Update 24/07/2023

--- src/script/keranjang.js
+++ src/script/keranjang.js
@@ -11,6 +11,24 @@ fetch(url)
   })
   .catch((error) => console.log(error));
 
+function input(checkbox) {
+  const productId = checkbox.dataset.productId;
+  console.log("data product id:", productId);
+
+  const deleteDeks = document.querySelector(".delete-deks");
+  const containerItem = document.querySelector(".keranjang-self");
+
+  if (checkbox.checked) {
+    deleteDeks.style.display = "flex";
+    containerItem.style.borderTopLeftRadius = "0";
+    containerItem.style.borderTopRightRadius = "0";
+  } else {
+    deleteDeks.style.display = "none";
+    containerItem.style.borderTopLeftRadius = "5px";
+    containerItem.style.borderTopRightRadius = "5px";
+  }
+}
+
 function renderProducts(cart) {
   const container = document.querySelector(".keranjang-pesanan ");
   container.innerHTML = "";
@@ -19,16 +37,16 @@ function renderProducts(cart) {
     const productHTML = `
     <div class="w-full">
       <div class="delete-cart justify-between items-center px-5 py-2 bg-green-600">
-        <input type="checkbox"  class="input-check-mobile w-5 h-5 outline-0" />
+        <input type="checkbox" class="input-check-mobile w-5 h-5 outline-0" />
         <button class="delete-mobile text-white hover:text-slate-800">Hapus</button>
       </div>
       <div class="delete-deks justify-between px-5 py-2 bg-green-600">
         <button class="button-hapus-deks text-white">Hapus</button>
       </div>
       <div
-      class="keranjang-self flex w-full justify-evenly items-center h-28 text-white bg-green-600"
+      class="keranjang-self flex w-full justify-evenly items-center h-28 text-white bg-green-600 " key=${item.id}
       >
-        <p class="chekbox-container"><input type="checkbox"  class="cart-checkbox w-5 h-5 outline-0" /></p>
+        <p class="chekbox-container"><input type="checkbox" data-product-id="${item.id}"  class="cart-checkbox w-5 h-5 outline-0" onclick={input(this)} /></p>
         <p>
           <img
             src=${item.image}
@@ -45,29 +63,10 @@ function renderProducts(cart) {
       `;
     container.insertAdjacentHTML("beforeend", productHTML);
   });
-
   deleteCart();
 }
 
 function deleteCart() {
-  const checkbox = document.querySelector(".cart-checkbox");
-  checkbox.addEventListener("change", deleteBar);
-
-  function deleteBar() {
-    const deleteCartDiv = document.querySelector(".delete-deks");
-    const containerItem = document.querySelector(".keranjang-self");
-
-    if (checkbox.checked) {
-      deleteCartDiv.style.display = "flex";
-      containerItem.style.borderTopLeftRadius = "0";
-      containerItem.style.borderTopRightRadius = "0";
-    } else {
-      deleteCartDiv.style.display = "none";
-      containerItem.style.borderTopLeftRadius = "5px";
-      containerItem.style.borderTopRightRadius = "5px";
-    }
-  }
-
   const checkMobile = document.querySelector(".input-check-mobile");
   checkMobile.addEventListener("change", deleteMobile);
 
